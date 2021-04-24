246a28033548f1031fbf9f3337aab75b2fd31c10
dkidanu1 <dkidanu@gmail.com>
2021-04-24 06:46:23 +0000

    Added map for contact form

--- static/js/maps.js
+++ static/js/maps.js
@@ -1,4 +1,4 @@
-var mymap = L.map('map').setView([53.3390102, -6.2641539], 13);
+var mymap = L.map('map').setView([59.3149, -18.0721], 13);
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 16,
@@ -8,4 +8,4 @@ var mymap = L.map('map').setView([53.3390102, -6.2641539], 13);
     accessToken: 'pk.eyJ1IjoiZGsxMjMzNDU1MzQzNCIsImEiOiJja252Y2xyaXcwNTg2MnB0Z2ZiOHdqNnB4In0.o2kPUM_qMpuLPwPYo_cXEA'
 }).addTo(mymap);
     var marker = L.marker([59.3149,18.0721]).addTo(mymap);
-    marker.bindPopup("<b>We are here!</b>").openPopup();
+    marker.bindPopup("<b>We are located here!</b>").openPopup();
