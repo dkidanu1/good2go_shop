9e0158659de0da2b5c94a42dc6f90da7d4d82cac
dkidanu1 <dkidanu@gmail.com>
2021-04-24 07:03:21 +0000

    Added Email Js to contact form

--- good2go_shop/settings.py
+++ good2go_shop/settings.py
@@ -177,12 +177,12 @@ STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'goodtogo@example.com'
-#else:
-    #EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    #EMAIL_USE_TLS = True
-    ##EMAIL_PORT = 
-    #EMAIL_HOST = 
-   # EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-   # EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-   # DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    #EMAIL_PORT = 
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
---
+++ static/js/email.js
@@ -0,0 +1,38 @@
+// --- Call function for email.js ---
+(function(){emailjs.init("user_xrIf5Dia8Ng6JpCHdTsuT");})();
+
+// --- Sends e-mail to set up service on email.js 
+document.getElementById('contactForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    emailjs.send("gmail", "contact", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "contact_request": contactForm.projectsummary.value
+    })
+        .then(
+            function success() {
+                notification();
+                setTimeout(refresh, 2500);
+            },
+            function failure() {
+                failToSend();
+            }
+        );
+    return false;
+});
+
+// --- Change text to notify user of successful send (200)  ---
+function notification() {
+    $("#submit").text("Your message has been submitted successfully");
+}
+
+// --- Refresh Form ---
+function refresh() {
+    $("#submit").text("Submit");
+    document.getElementById("contactForm").reset();
+}
+
+// --- E-mail failed to sent (404) ---
+function failToSend(){
+    $("#submit").text("Failed to submit. please refresh page and try again");
+}
