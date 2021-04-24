1ea311969737c4749821dbe561b8344602871362
dkidanu1 <dkidanu@gmail.com>
2021-04-24 10:04:52 +0000

    Added Map and Map marker

--- contact/urls.py
+++ contact/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
 from . import views
 
-from .views import contactView, successView
-
 urlpatterns = [
-    path('contact/', contactView, name='contact'),
-    path('success/', successView, name='success'),
+    path('', views.index, name='contact'),
 ]
--- static/js/maps.js
+++ static/js/maps.js
@@ -1,11 +1,15 @@
-var mymap = L.map('map').setView([59.3149, -18.0721], 13);
-    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 16,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoiZGsxMjMzNDU1MzQzNCIsImEiOiJja252Y2xyaXcwNTg2MnB0Z2ZiOHdqNnB4In0.o2kPUM_qMpuLPwPYo_cXEA'
-}).addTo(mymap);
-    var marker = L.marker([59.3149,18.0721]).addTo(mymap);
-    marker.bindPopup("<b>We are located here!</b>").openPopup();
+console.log("LOADING MAPS.JS")
+mapboxgl.accessToken = 'pk.eyJ1IjoiZGsxMjMzNDU1MzQzNCIsImEiOiJja252ZXRyc24wNHk3Mm9wbTF6NTYwNDRsIn0.1H5llGIQ0fJGyXovrPh0PA';
+var map = new mapboxgl.Map({
+container: 'map', // container id
+style: 'mapbox://styles/mapbox/streets-v11', // style URL
+center: [18.06913, 59.33301], // starting position [lng, lat]
+zoom: 7 // starting zoom
+});
+
+// Set options
+var marker = new mapboxgl.Marker({
+    color: "#FFFFFF",
+    draggable: true
+  }).setLngLat([18.0614, 59.3323])
+  .addTo(map);
