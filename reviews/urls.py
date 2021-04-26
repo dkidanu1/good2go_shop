19b665eb413cb8aee928f3e917ee48e0df3685e0
dkidanu1 <dkidanu@gmail.com>
2021-04-26 17:39:10 +0000

    Add product view, url and template

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.reviews, name='reviews'),
-    path('add_comment/<product_id>', views.add_comment, name="add_comment"),
-    path('edit_comment/<product_id>', views.edit_comment, name="edit_comment"),
-    path('delete_comment/<product_id>', views.delete_comment, name="delete_comment"),
+    path('add_comment/<int:product_id>', views.add_comment, name="add_comment"),
+    path('edit_comment/<int:product_id>', views.edit_comment, name="edit_comment"),
+    path('delete_comment/<int:product_id>', views.delete_comment, name="delete_comment"),
 ]
