2be82df8f4cd0bfe0ed2a58d5a73275b7298013c
dkidanu1 <dkidanu@gmail.com>
2021-04-27 19:00:16 +0000

    Added stripe variable in settings

--- good2go_shop/settings.py
+++ good2go_shop/settings.py
@@ -206,6 +206,8 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+DEFAULT_FROM_EMAIL = 'goodtogo@example.com'
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
